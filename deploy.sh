9535affc6e773c3f0cbcec4c6aeb6513bd777bb4
ryandavidhartman <ryandavidhartman@gmail.com>
2025-03-02 19:58:59 -0500

    remove acerak

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cp -TRv ./docs/* /home/ryandavidhartman/dev/source/heroku/oncewas-rpg
+cd /home/ryandavidhartman/dev/source/heroku/oncewas-rpg
+rm home.html
+mv index.html home.html
+git add *
+git commit
