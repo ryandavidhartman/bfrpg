4a450c4ed9035f7e9e2b5a87b6478d812992ed32
ryandavidhartman <ryandavidhartman@gmail.com>
2025-11-01 00:50:47 -0400

    adding a basics section

--- docs/custom.js
+++ docs/custom.js
@@ -651,28 +651,30 @@ export function rollIndependentColumns(tableSelector, columns, diceSpecs = null,
 export function calculateModifier (stat) {
   switch (stat) {
     case 3:
-      return "-3"
+      return "-4"
     case 4: 
     case 5: 
-      return "-2"
+      return "-3"
     case 6: 
     case 7:
-    case 8: 
-      return "-1"
+      return "-2"
+    case 8:  
     case 9:
+      return "-1"
     case 10:
-    case 11: 
+    case 11:
+      return "0" 
     case 12:
-      return "0"
     case 13:
+      return "+1"
     case 14:
     case 15: 
-      return "+1"
+      return "+2"
     case 16: 
     case 17: 
-      return "+2"
-    case 18: 
       return "+3"
+    case 18: 
+      return "+4"
     default:
       return "+3"
   }
